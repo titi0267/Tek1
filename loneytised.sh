aacae5e587b275b1ec46e1491cb6786b7265b099
Timothé Coniel <coniel@localhost.localdomain>
2020-09-29 18:32:56 +0200

    Task05 amen

---
+++ loneytised.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sed -e 's/theol/Wile E. Coyote/' | sed -e 's/stevenl/Daffy Duck/' | sed -e 's/arnaud1/Porky Pig/' | sed -e 's/pierre-jean/Marvin the Martian/' | grep "Wile E. Coyote\|Daffy Duck\|Porky Pig\|Marvin the Martian"
