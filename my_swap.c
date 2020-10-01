bd575d36e9db6af2aae1e0e4d1b850e6ceca6e6f
Timothé Coniel <coniel@localhost.localdomain>
2020-10-01 11:39:07 +0200

    task1

---
+++ my_swap.c
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2020
+** B-CPE-100-STG-1-1-cpoolday03-timothe.coniel
+** File description:
+** my_swap
+*/
+#include <unistd.h>
+
+void my_swap(int *a, int *b)
+{
+    int c = *a;
+
+    *a = *b;
+    *b = c;
+}
