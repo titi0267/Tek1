26c8820ebfed802e66363baf613126056be8ff0c
Timothé Coniel <coniel@localhost.localdomain>
2020-10-02 14:15:16 +0200

    task1 modif + as int

--- my_compute_factorial_it.c
+++ my_compute_factorial_it.c
@@ -2,7 +2,7 @@
 ** EPITECH PROJECT, 2020
 ** B-CPE-100-STG-1-1-cpoolday05-timothe.coniel
 ** File description:
-** my_computer_factorial_it
+** my_compute_factorial_it
 */
 
 int my_compute_factorial_it(int nb)
@@ -10,10 +10,10 @@ int my_compute_factorial_it(int nb)
     int end_nb = 1;
     int b = 1;
 
-    if (nb == 1) {
+    if (nb == 1 || nb == 0) {
         b = 1;
-    } else if (nb == 0) {
-        b = 1;
+    } else if (nb < 2147483648 || nb < 0) {
+        b = 0;
     } else {
         while (end_nb != nb + 1) {
             b = b * end_nb;
