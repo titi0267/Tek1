4c39e90e3599565dd5284c94c270c1c4acb7d916
Timothé Coniel <coniel@localhost.localdomain>
2020-10-02 13:36:04 +0200

    task2

--- my_putstr.c
+++ my_putstr.c
@@ -10,9 +10,9 @@ int my_putstr(char const *str)
     char carac;
 
     carac = 0;
-    while (str[carac] != '\0' ) 
-    {
+    while (str[carac] != '\0' ) {
         my_putchar(str[carac]);
         carac++;
     }
+    return (str);
 }
