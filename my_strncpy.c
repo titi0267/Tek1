0c480fc38979016719690b6236e6b50293cc5e4f
Timothé Coniel <coniel@localhost.localdomain>
2020-10-05 20:12:13 +0200

    Task 2

--- my_strncpy.c
+++ my_strncpy.c
@@ -5,32 +5,34 @@
 ** my_strncpy
 */
 
-int my_strlen(char const *src)
+int my_strlen(char *src)
 {
     int carac = 0;
 
-    while (src[carac] != '\0') 
-        carac = carac + 1;
+    while (src[carac] != '\0')
+    {
+        carac += 1;
+    }
     return (carac);
 }
 
-int *my_strncpy(char *dest, char const *src, int n)
+char *my_strncpy(char *dest, char const *src, int n)
 {
-    int i;
-    int b = my_strlen(src);
+    int i = 0;
+    int carac = my_strlen(src);
 
-    if (n > b) {
-        for (i = 0; src[i] != '\0'; i += 1) {
-            dest[i] = src[i];
+    if (n > carac) {
+        for (i = 0; src[i] != '\0' && i < n; i += 1) {
+            dest[i] = src[i]; 
         }
         dest[i] = '\0';
         return (dest);
-    }
+    } 
     else {
-        for (i = 0; src[i] != '\0'; i += 1) {
+        for (i = 0; src[i] != '\0' && i < n; i += 1) {
             dest[i] = src[i];
         }
         return (dest);
     }
-    
 }
+
