1cb02dcf2a5148a91d30196c8cfea1c5b543bf22
Timothé Coniel <coniel@localhost.localdomain>
2020-10-06 01:20:10 +0200

    task5

---
+++ my_strstr.c
@@ -0,0 +1,50 @@
+/*
+** EPITECH PROJECT, 2020
+** my_strstr
+** File description:
+** 
+*/
+#include <string.h>
+
+int my_strlen(char const *str)
+{
+    int carac = 0;
+
+    while (str[carac] != '\0')
+        carac = carac + 1;
+    return (carac);
+}
+
+int my_strncmp(char *str, char const *to_find, int a)
+{
+    int b = 0;
+    int nb = my_strlen(to_find);
+    int res;
+
+    while (str[a] != '\0' && to_find[b] != '\0') {
+        if (str[a] == to_find[b]) {
+            a += 1;
+            b += 1;
+        }
+        if (b == nb)
+            return (1);
+        else if (str[a] > to_find[b])
+            return (0);
+    }
+}
+
+char *my_strstr(char *str , char  const *to_find)
+{
+    int a = 0;
+    int b = 0;
+    int res = 0;
+
+    for (; str[a] != '\0'; a += 1) {
+        if (str[a] == to_find[b]) {
+            res = my_strncmp(str, to_find, a);
+        }
+        if (res == 1)
+            return (&str[a]);
+    }
+    return (NULL);
+}
