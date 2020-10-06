ffa44cc9c64f926bc7a2b05f6b87cf6e7f36b35f
Timothé Coniel <coniel@localhost.localdomain>
2020-10-06 15:49:48 +0200

    task5

--- my_strcmp.c
+++ my_strcmp.c
@@ -5,64 +5,16 @@
 ** 
 */
 
-/*int my_strlen(char const *str)
+int my_strcmp(char *s1, char const *s2)
 {
-    int carac = 0;
+    int a;
+    int res;
 
-    while (str[carac] != '\0')
-        carac = carac + 1;
-    return (carac);
-}
-
-int my_strstr(char const *s1, char const *s2)
-{
-    int a = 0;
-    int b = 0;
-    int nb = my_strlen(s2);
-
-    while (s1[a] != '\0' && s2[b] != '\0') {
-        if (s1[a] == s2[b]) {
-            a += 1;
-            b += 1;
-        }
-        if (b == nb)
-            return (0);
-        else if (s1[a] > s2[b])
-            return (1);
-        else
-            return (-1);
-        }
-}
-
-char *my_strcmp(char *s1 , char  const *s2)
-{
-    int a = 0;
-    int b = 0;
-    int res = 0;
-
-    for (; s1[a] != '\0'; a += 1) {
-        if (s1[a] == s2[b]) {
-            res = my_strstr(s1, s2);
-        
-        }
-        if (res == 1) {
-            return (1);
-        else if (res == 0) {
-        }
-            return (0);
-        }
-        else {
-            return (-1);
-            printf("IN");
-        }
+    for (a = 0; s1[a] == s2[a] && s1[a] != '\0' && s2[a] != '\0'; a += 1) {
+        if (s1[a] < s2[a] || s1[a] > s2[a])
+            res = 0;
     }
-    return (res);
+    if (s1[a] == s2[a])
+        res = 1;
+    return(res);
 }
-
-int main(void)
-{
-    char s1[] = "printf de la string interdit";
-    char s2[] = "lolo";
-
-    printf(my_strcmp(s1, s2));
-}*/
