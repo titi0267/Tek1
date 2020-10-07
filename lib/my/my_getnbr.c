cf4271629ffa9fbf27fe13ce98469025097d7ac3
Timothé Coniel <coniel@localhost.localdomain>
2020-10-07 16:55:15 +0200

    lib test number one

---
+++ lib/my/my_getnbr.c
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2020
+** my_getnbr
+** File description:
+** 
+*/
+
+int my_getnbr(char const *str)
+{
+    return (0);
+}
---
+++ lib/my/my_strcat.c
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2020
+** my_strcat
+** File description:
+** 
+*/
+
+ char *my_strcat(char *dest, char const *src)
+ {
+     return (0);
+ }
