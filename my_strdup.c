1d03cdea95198b4da71cc5b7ece89f62f11075ba
Timothé Coniel <coniel@localhost.localdomain>
2020-10-08 11:18:17 +0200

    task1 malloc

---
+++ my_strdup.c
@@ -0,0 +1,25 @@
+/*
+** EPITECH PROJECT, 2020
+** my_strdup
+** File description:
+** 
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int my_strlen(char const *str);
+
+char *my_strdup(char const *src)
+{
+    int i = 0;
+    char *str;
+    int c = my_strlen(src);
+
+    str = malloc(sizeof(char) * (c + 1));
+    while (str[i] < c) {
+        str[i] = src[i];
+        i += 1;
+    }
+    str[i] = '\0';
+    return (str);
+}
