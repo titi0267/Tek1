c401c5fdb4c839f25f2b93171824c88a79bec66c
Timothé Coniel <coniel@localhost.localdomain>
2020-10-12 09:52:41 +0200

    task1

---
+++ include/my_macro_abs.h
@@ -0,0 +1,10 @@
+/*
+** EPITECH PROJECT, 2020
+** Untitled (Workspace)
+** File description:
+** my_macro_abs
+*/
+#ifndef
+#define ABS
+#define ABS(value) ((value < 0) ? (value * -1) : (value))
+#endif
