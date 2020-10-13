21e3d1c52636616cdf2f1caf48aeecce09cb7d7f
Timothé Coniel <coniel@localhost.localdomain>
2020-10-13 10:07:37 +0200

    task1 makefile

---
+++ lib/my/my_putchar.c
@@ -0,0 +1,12 @@
+/*
+** EPITECH PROJECT, 2020
+** my_putchar
+** File description:
+** 
+*/
+
+void my_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
---
+++ lib/my/my_str_islower.c
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2020
+** my_str_islower
+** File description:
+** 
+*/
+
+int my_str_islower(char const *str)
+{
+    return (0);
+}
