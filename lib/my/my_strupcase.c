135d18a5039565bec8cee466cbcdf79c035c8ba6
Timothé Coniel <coniel@localhost.localdomain>
2020-10-13 20:41:54 +0200

    task1

---
+++ lib/my/my_strupcase.c
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2020
+** my_strupcase
+** File description:
+** my_strupcase
+*/
+
+char *my_strupcase(char *str)
+{
+    int a = 0;
+
+    for (; str[a] != '\0'; a += 1) {
+        if (str[a] >= 97 && str[a] <= 122) {
+            str[a] = str[a] - 32;
+        }
+    }
+    return (str);
+}
