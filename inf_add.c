69afa98998574901104dd40cfdd377ba5d04b1c7
Timothé Coniel <coniel@localhost.localdomain>
2020-10-16 22:42:30 +0200

    inf_add functional, pb with 99 + 1

--- inf_add.c
+++ inf_add.c
@@ -13,19 +13,19 @@ char *add(char *gauche, char *droite)
 {
     int retenue = 0;
     int i = 0;
-    char *res = malloc(sizeof(char) * (my_strlen(gauche) + my_strlen(droite) + 8));
+    char *res = malloc(sizeof(char) * (my_strlen(res) + 4));
 
     gauche = my_revstr(gauche);
     droite = my_revstr(droite);
     for (; droite[i] != '\0'; i += 1) {
         int add = gauche[i] + droite[i] - 48 - 48;
-        if (add >= 10) {
-            res[i] = add - 10 + 48 + retenue;
-            retenue = 1;
-        } else {
-            res[i] = add + 48 + retenue;
-            retenue = 0;
-        }
+    if (add >= 10) {
+        res[i] = add - 10 + 48 + retenue;
+        retenue = 1;
+    } else {
+        res[i] = add + 48 + retenue;
+        retenue = 0;
+    }
     }
     for (; gauche[i] != '\0'; i += 1) {
         int add = gauche[i] - 48;
@@ -35,8 +35,11 @@ char *add(char *gauche, char *droite)
         } else {
             res[i] = add + 48 + retenue;
             retenue = 0;
-
         }
+    }
+    if (retenue == 1) {
+        res[i] = 49;
+        i += 1;
     }
     res[i] = '\0';
     return (my_revstr(res));
@@ -51,9 +54,11 @@ int main(int ac, char **av)
     if (gauche < droite) {
         droite = av[1];
         gauche = av[2];
-    } else {
+    }
+    else if (gauche >= droite) {
         gauche = av[1];
         droite = av[2];
     }
+
     my_putstr(add(gauche, droite));
 }
