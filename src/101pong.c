eccbf2e6ef6a9147c734e8dfb2514fe5b29b423a
Timothé Coniel <timothe@coniel.com>
2020-11-06 17:25:48 +0100

    error system for number of arguments and -h

--- src/101pong.c
+++ src/101pong.c
@@ -6,8 +6,34 @@
 */
 
 #include "../include/my.h"
+#include <stddef.h>
+
+int check_error(void)
+{
+    my_putstr("Invalid Syntax");
+    return (84);
+}
+
+void check_h(void)
+{
+    my_putstr("USAGE\n");
+    my_putstr("     ./101pong x0 y0 z0 x1 y1 z1 n\n\n");
+    my_putstr("DESCRIPTION\n");
+    my_putstr("     x0  ball abscissa at time t -1\n");
+    my_putstr("     y0  ball ordinate at time t -1\n");
+    my_putstr("     z0  ball altitude at time t -1\n");
+    my_putstr("     x1  ball abscissa at time t\n");
+    my_putstr("     y1  ball ordinate at time t\n");
+    my_putstr("     z1  ball altitude at time t\n");
+    my_putstr("     n   time shift (greater than or equal to zero, integer)\n");
+}
 
 int main(int ac, char **av)
 {
-    int a = 0;
+    if (av[1] != NULL && my_strcmp(av[1], "-h") == 0) {
+        check_h();
+        return (0);
+    }
+    if (ac != 8 && my_strcmp(av[1], "-h") != 0)
+        check_error();
 }
