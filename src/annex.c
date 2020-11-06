4bfe5d829d281b2c09ef66d3f3bc7b63b36b80a3
Timothé Coniel <timothe@coniel.com>
2020-11-06 22:08:25 +0100

    velocity vector calculations done, right display done

--- src/annex.c
+++ src/annex.c
@@ -14,5 +14,23 @@
 
 void core101(infin_number_t *info)
 {
+    int c = 3;
     int a = 0;
+    float f = 0;
+
+    my_putstr("The Velocity vector of this ball is :\n");
+    for (;c < 8 && a < 3; a ++, c ++) {
+        if (c == 3 && a == 0) {
+            f = info->given[c] - info->given[a];
+            printf("(%.2f, ", f);
+        }
+        if (c == 4 && a == 1) {
+            f = info->given[c] - info->given[a];
+            printf("%.2f, ", f);
+        }
+        if (c == 5 && a == 2) {
+            f = info->given[c] - info->given[a];
+            printf("%.2f)\n", f);
+        }
+    }
 }
