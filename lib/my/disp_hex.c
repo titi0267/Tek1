7b618b6226bfe692c2df1326c4a6b26f0fcfc39d
Timothé Coniel <timothe@coniel.com>
2020-11-15 23:24:55 +0100

    print converted hex on maj and min caracters, print non-ascii caracters

--- lib/my/disp_hex.c
+++ lib/my/disp_hex.c
@@ -4,15 +4,20 @@
 ** File description:
 ** disp_hex
 */
-
 #include "my.h"
 #include "my_struct.h"
 #include <stdarg.h>
 
+void disp_heX(va_list *argu_list, str_len_t *info)
+{
+    unsigned int e = va_arg(*argu_list, unsigned int);
+
+    convert_heX(e, info);
+}
+
 void disp_hex(va_list *argu_list, str_len_t *info)
 {
-    int e = va_arg(*argu_list, int);
+    unsigned int e = va_arg(*argu_list, unsigned int);
 
     convert_hex(e, info);
-    //my_putstr(f, info);
 }
