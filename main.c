3ef52545fa6367dca3fc4e795a374078da0aefa6
Timothé Coniel <timothe@coniel.com>
2020-11-19 15:58:19 +0100

    main

--- main.c
+++ main.c
@@ -16,34 +16,25 @@ int main(int ac, char **av)
 {
     int fd = fs_open_file(av[1]);
     int size;
-    char *buffer;// = malloc(sizeof(char) * size);
-    //int x = 0;
-    //int w = 0;
+    char *buffer;
     buffer_size_t *info = malloc(sizeof(buffer_size_t));
     struct stat buff;
 
-    fstat(fd, &buff);
+    stat(av[1], &buff);
     size = buff.st_size;
+    //if (stat(av[1], &buff) == -1)
+    //  return (NULL);
     buffer = malloc(sizeof(char) * size + 1);
     fs_understand_return_of_read(fd, buffer, size);
-    //for (int i = 0; buffer[i] != '\0'; i++) {
-    dot_pos(buffer, info);
-    //}
-    my_put_nbr(info->store_btw);
-    my_putchar('\n');
-    my_put_nbr(info->store_nbrbuf);
-    my_putchar('\n');
-    //my_put_nbr(info->store_nbrline);
+    //calc(buffer, info);
     //my_putchar('\n');
-    my_put_nbr(info->carac_l);
-    my_putchar('\n');
-    my_put_nbr(info->before_n);
-    my_putchar('\n');
-    my_put_nbr(info->after_n);
-    my_putchar('\n');
+    //my_putchar(buffer[2]);
+    //my_putchar(buffer[3]);
+    //my_putchar(buffer[100]);
+    //my_putchar(buffer[13]);
+    //my_put_nbr(info->carac_l);
     write(1, buffer, size);
-    //my_putchar(buffer[19]);
-    //my_putstr(buffer);
     close(fd);
-    //printf("%d\n", info->store_btw);
+    free(buffer);
+    return (0);
 }
