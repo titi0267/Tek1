c404a77d57d8039995af7381656b634e5d7d9263
Elliot Waldvogel <elliotwaldvogel@localhost.localdomain>
2020-11-30 19:20:02 +0100

    Pushing roots of cipher project

---
+++ src/cipher.c
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2020
+** cipher.c by Timothy CONIEL & Elliot WALDVOGEL
+** File description:
+** cipher
+*/
+
+#include "../include/my.h"
+#include "../include/my_struct.h"
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int ac, char **av)
+{
+    infin_number_t *info = malloc(sizeof(infin_number_t));
+
+    return (0);
+}
