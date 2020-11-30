1b1942c5b6e8bba541da45a073765972b2c6bf3c
Elliot Waldvogel <elliotwaldvogel@localhost.localdomain>
2020-11-30 19:14:30 +0100

    Pushing roots of project

---
+++ include/my_struct.h
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2020
+** my_struct for the infin_add.c project by Elliot WALDVOGEL
+** File description:
+** my_struct
+*/
+
+#include <unistd.h>
+
+#ifndef MY_STRUCT_H_
+#define MY_STRUCT_H_
+
+typedef struct infin_number
+{
+    int amount;
+} infin_number_t;
+
+#endif /* !MY_STRUCT_H_ */
---
+++ src/navy.c
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2020
+** navy.c by Timothy CONIEL & Elliot WALDVOGEL
+** File description:
+** navy
+*/
+
+#include "../include/my.h"
+#include "../include/my_struct.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int ac, char **av)
+{
+    infin_number_t *info = malloc(sizeof(infin_number_t));
+}
