83df9362743e2e5301bb404b96c10496a07c4fa3
Timothé Coniel <timothe@coniel.com>
2020-12-15 23:04:19 +0100

    -help

--- src/navy.c
+++ src/navy.c
@@ -10,9 +10,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+void help(int ac, char **av)
+{
+    if (ac == 2 && (av[1][0] == '-' && av[1][1] == 'h')) {
+        my_printf("USAGE\n      ./navy [first_player_pid] navy_positions\n");
+        my_printf("DESCRIPTION\n      first_player_pid: only for the 2nd ");
+        my_printf("player. pid of the first player.\n      navy_positions: ");
+        my_printf("file representing the positions of the ships.\n");
+    }
+}
+
 int main(int ac, char **av)
 {
     infin_number_t *info = malloc(sizeof(infin_number_t));
 
+    help(ac, av);
     return (0);
 }
