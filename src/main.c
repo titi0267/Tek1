f93f51778f29d80972b6603ad4ad1c1511fcc386
Timothé Coniel <timothe@coniel.com>
2020-12-23 21:04:49 +0100

    File name

---
+++ src/main.c
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2020
+** B-MUL-100-STG-1-1-myrunner-timothe.coniel
+** File description:
+** test
+*/
+#include "../include/my.h"
+#include "../include/my_struct.h"
+
+int main(void)
+{
+    map_t *buff = malloc(sizeof(map_t));
+    print_map(buff);
+    for (int i = 0; i != 10; i++)
+        free(buff->line[i]);
+    free(buff->line);
+    free(buff->buffer);
+    free(buff);
+}
