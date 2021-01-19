b25cd9d51306dbb9fea02ab67c396c8cb24a245c
Timothé Coniel <timothe@coniel.com>
2021-01-19 14:29:25 +0100

    root

---
+++ src/main.c
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2021
+** solo_stumper by Timothy CONIEL
+** File description:
+** solo_stumper.c
+*/
+
+#include "../include/my.h"
+
+int main(int ac, char **av)
+{
+    infin_number_t *info = malloc(sizeof(infin_number_t));
+
+    if (info == NULL)
+        return (84);
+    return (0);
+}
---
+++ src/next_main.c
@@ -0,0 +1,9 @@
+/*
+** EPITECH PROJECT, 2021
+** solo_stumper by Timothy CONIEL
+** File description:
+** solo_stumper.c
+*/
+
+#include "../include/my.h"
+
