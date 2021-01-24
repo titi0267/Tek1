32aed14a916feec2b479a8885ff522c62967ee98
Timothé Coniel <timothe@coniel.com>
2021-01-24 13:38:17 +0100

    store tree key

--- giant_src/recreate_tree.c
+++ giant_src/recreate_tree.c
@@ -10,33 +10,36 @@
 int recover_freq(giant_t *buffer)
 {
     int i = 0;
-    int x = 0;
+    int x = 1;
     int store = 0;
     int second = 0;
     int y = 0;
 
     buffer->le_freq = malloc(sizeof(int) * buffer->keysize + 1);
     buffer->in_order = malloc(sizeof(char) * buffer->keysize + 1);
+    buffer->in_order[0] = buffer->key[0];
     for (; buffer->key[i] != '\0'; i++) {
-        if ((buffer->key[i] != '\0' || buffer->key[i + 1] != '\0') && buffer->key[i + 1] == 6)
+        if (buffer->key[i - 1] == 6) {
             buffer->in_order[x] = buffer->key[i];
-        if ((buffer->key[i] <= 57 && buffer->key[i] >= 48) && (buffer->key[i + 1] <= 57 && buffer->key[i + 1] >= 48)) {
-            while (buffer->key[i + 1] != '\0') {
+            x++;
+        }
+        if (buffer->key[i] >= '0' && buffer->key[i] <= '9') {
+            while (buffer->key[i] != 6) {
                 second = store;
-                store = buffer->key[i] - 48;
-                if ((buffer->key[i + 1] < 48 && buffer->key[i + 1] > 57)) {
-                    second = second + (buffer->key[i] - 48);
+                store = buffer->key[i] - '0';
+                if (buffer->key[i + 1] > 57 || buffer->key[i + 1] < 48) {
+                    second = second + (buffer->key[i] - '0');
                     break;
-                }
-                else
+                } else
                     store *= 10;
                 i++;
             }
             buffer->le_freq[y] = second;
+            store = 0;
             y++;
         }
-        if ((buffer->key[i] != '\0' || buffer->key[i + 1] != '\0') && buffer->key[i + 1] <= 57 && buffer->key[i + 1] >= 48)
-            buffer->le_freq[x] = buffer->key[i];
     }
+    buffer->le_freq[y] = '\0';
+    buffer->in_order[x] = '\0';
     return (0);
 }
