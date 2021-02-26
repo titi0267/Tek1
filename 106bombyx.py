300584d2c73a3104b81b21a9b4132ae6eab38137
Timothe Coniel <timothe@coniel.com>
2021-02-26 16:53:26 +0100

    error handling

--- 106bombyx.py
+++ 106bombyx.py
@@ -25,23 +25,29 @@ def nbr_arg():
     elif len(sys.argv) == 4:
         return (1)
     else:
+        help()
         return (-1)
 
 def two_arg(n, k):
-    x = float(n)
-    y = float(n)
-    print("1 %.2f" % float(n))
-    for i in range(2, 101):
-        x = (float(k) * y) * ((1000 - y) / 1000)
-        print(str(i) + " " + str("%.2f" % x))
-        y = x
+    try:
+        x = float(n)
+        y = float(n)
+        print("1 %.2f" % float(n))
+        for i in range(2, 101):
+            x = (float(k) * y) * ((1000 - y) / 1000)
+            print(str(i) + " " + str("%.2f" % x))
+            y = x
+    except ValueError:
+        exit (84)
 
 def three_arg(n):
-    x = float(n)
-    y = float(n)
-    i0 = sys.argv[2]
-    i1 = sys.argv[3]
-    #print(str("%.2f" % float(k)) + str(" %.2f" % float(x)))
+    try:
+        x = float(n)
+        y = float(n)
+        i0 = sys.argv[2]
+        i1 = sys.argv[3]
+    except ValueError:
+        exit (84)
     for k in range(100, 401):
         for i in range(1, int(i0)):
             x = ((k / 100) * y) * ((1000 - y) / 1000)
@@ -52,15 +58,16 @@ def three_arg(n):
             y = x
         k += 1
 
-
 if __name__ == '__main__':
     i = nbr_arg()
     if i != 1 and i != 0:
         exit(84)
-    n = sys.argv[1]
-    k = sys.argv[2]
+    try:
+        n = sys.argv[1]
+        k = sys.argv[2]
+    except ValueError:
+        exit (84)
     if i == 0:
         two_arg(n, k)
     if i == 1:
         three_arg(n)
-    help()
