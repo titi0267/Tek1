14a3e306b4a806030158c54069d3cfb8898ef807
Timothe Coniel <timothe@coniel.com>
2021-02-26 14:17:28 +0100

    help

--- src/main.py
+++ src/main.py
@@ -5,3 +5,33 @@
 ## 106bombyx.py
 ##
 
+import sys
+
+def help():
+    try :
+        if sys.argv[1] == "-h" and len(sys.argv) == 2:
+            print('USAGE\n    ./106bombyx n [k | i0 i1]')
+            print('DESCRIPTION')
+            print('    n      number of first generation individuals')
+            print('    k      growth rate from 1 to 4')
+            print('    i0     initial generation (included)')
+            print('    i1     final generation (included)')
+    except:
+        IndexError
+
+def suit_len():
+    try:
+        x = sys.argv[1]
+        y = ''.join(x)
+    except:
+        TypeError
+    z = int(y)
+    s = 0
+    for i in range(0, z):
+        s += 1
+        print('', s)
+        if int(s) >= int(z):
+            break
+
+suit_len()
+help()
