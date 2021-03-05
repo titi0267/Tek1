d904a0421c0cb7a548c5a24b623a2d95e42d885d
Timothe Coniel <timothe@coniel.com>
2021-03-05 15:07:36 +0100

    fractal two

---
+++ src/fractal_two.c
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2021
+** duo stumper
+** File description:
+** fractal2.c
+*/
+
+#include "../include/my.h"
+
+int parse_str_sec(char **av)
+{
+    int f = my_getnbr(av[1]);
+    
+    for (int i = 0; av[2][i] != '\0'; i++) {
+        for (int d = 0; av[2][d] != '@'; d++) {
+                my_putchar(av[2[i]);
+        }
+                    if ()
+                        
+    }
+}
