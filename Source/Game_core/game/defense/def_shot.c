096e2a221bed8e9d133dbe8a0fd88ec670b6d6e9
Timothe Coniel <timothe@coniel.com>
2021-03-18 00:22:58 +0100

    enemy pass near tower

--- Source/Game_core/game/defense/def_shot.c
+++ Source/Game_core/game/defense/def_shot.c
@@ -12,15 +12,16 @@ void range_def(game_t *game, enemy_t *enemy)
 {
     defense_t *defense_bis = *(game->defense);
     pirat_data_t *data = *(enemy->data);
-    sfFloatRect build = sfSprite_getGlobalBounds(game->defense_bis->new_build);
-    sfFloatRect bad_guy = sfSprite_getGlobalBounds(enemy->data_bis->pirat_walk);
+    sfFloatRect build;
+    sfFloatRect bad_guy;
 
     for (; defense_bis != NULL; defense_bis = defense_bis->next) {
-        int i = 0;
-        for (; data != NULL; data = data->next) {
-            /*if (build.left + build.width < bad_guy.left + 100 && ) {
-            printf("tower = %f, enemy = %f\n", game->defense_bis->pos.x, enemy->data_bis->pos.x);
-            }*/
+        data = *(enemy->data);
+        build = sfSprite_getGlobalBounds(defense_bis->new_build);
+        for (int i = 0;  i < game->nb_spt && data != NULL; data = data->next, i++) {
+            bad_guy = sfSprite_getGlobalBounds(data->pirat_walk);
+            if ((bad_guy.left < build.left + build.width + 50 && bad_guy.left > build.left - 50) && (bad_guy.top < build.top + build.height + 50 && bad_guy.top > build.top - 50))
+                printf("tower = %f, enemy = %f & %f & %i & %i\n", defense_bis->pos.y, data->pos.x, data->pos.y, data->nb_pirat, i);
         }
     }
 }
