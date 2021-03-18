ce7d59be9a3f91544d2a244c9dcdb17a24842eea
Tom <tom@localhost.localdomain>
2021-03-18 15:28:02 +0100

    la noooorme

--- src/map.c
+++ src/map.c
@@ -1,6 +1,6 @@
 /*
 ** EPITECH PROJECT, 2020
-** Map by Timothy CONIEL & Elliot WALDVOGEL
+** Map by Timothy CONIEL & Tom NOMINE
 ** File description:
 ** map.c
 */
@@ -17,19 +17,19 @@ int read_map(duo_stp_t *map, char **av)
     int fd = open(av[1], O_RDONLY);
 
     if (fd == -1) {
-        write (2, "wrong file\n", 12);
+        write(2, "wrong file\n", 12);
         return (84);
     }
     map->buffer_size = 1800;
     map->buffer = malloc(sizeof(char) * map->buffer_size + 1);
     read_ret = read(fd, map->buffer, map->buffer_size);
     if (read_ret == -1) {
-        write (2, "wrong read\n", 12);
+        write(2, "wrong read\n", 12);
         return (84);
     }
     map->buffer_size = read_ret;
     map->buffer[map->buffer_size] = '\0';
-    close (fd);
+    close(fd);
     map->line_nbr = nbr_line(map->buffer);
     map->char_nbr = nbr_char(map->buffer);
     return (0);
