5711e685b6a491d8e9d3746c886b9ca2d5fc965c
Tom <tom@localhost.localdomain>
2021-03-18 15:54:08 +0100

    delete inutile fonction for LA NORME

--- include/my.h
+++ include/my.h
@@ -16,8 +16,6 @@
 
 void my_putchar(char c);
 int my_putstr(char const *str);
-int my_strncmp(char *cmp, char *cmpd);
-int my_putnbr(int nb);
 int my_getnbr(char *str);
 int read_map(duo_stp_t *map, char **av);
 int store_map(duo_stp_t *map);
