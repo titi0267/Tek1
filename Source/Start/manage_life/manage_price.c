a8cf38ca304921ea6d5003d751c1b4818e72a083
tristan <tristan.hourtoulle@epitech.eu>
2021-03-21 22:35:54 +0100

    correct price

--- Source/Start/manage_life/manage_price.c
+++ Source/Start/manage_life/manage_price.c
@@ -31,7 +31,7 @@ void manage_price_three(core_t *core)
 {
     lib_t *lib = malloc(sizeof(lib_t));
     sfFont *Font = sfFont_createFromFile("Ressources/FONT/Early GameBoy.ttf");
-    int i = core->shop->price2;
+    int i = core->shop->price3;
     char *scr = my_int_str(i, lib);
     sfColor color = sfBlack;
     sfVector2f pos_score = {1540, 460};
@@ -51,7 +51,7 @@ void manage_price_two(core_t *core)
 {
     lib_t *lib = malloc(sizeof(lib_t));
     sfFont *Font = sfFont_createFromFile("Ressources/FONT/Early GameBoy.ttf");
-    int i = core->shop->price3;
+    int i = core->shop->price2;
     char *scr = my_int_str(i, lib);
     sfColor color = sfBlack;
     sfVector2f pos_score = {1540, 670};
