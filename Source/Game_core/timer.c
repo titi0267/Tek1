00ce1d7021b35f2dd7afb6a1c48d877991b0cf38
Timothe Coniel <timothe@coniel.com>
2021-03-21 18:09:53 +0100

    giga merde arrived

--- Source/Game_core/timer.c
+++ Source/Game_core/timer.c
@@ -7,8 +7,18 @@
 
 #include "../../include/defender.h"
 
-void update_time(time_t *time)
+void update_time(core_t *core)
 {
-    time->delta_time = sfClock_restart(time->clock).microseconds / 1000000.0;
-    time->elapsed_time += time->delta_time;
+    if (core->menu->pause_on == FALSE) {
+        if (core->time->recreate_clock == TRUE) {
+            core->time->clock = sfClock_create();
+            core->time->delta_time = 0;
+            core->time->recreate_clock = FALSE;
+        }
+        core->time->delta_time = sfClock_restart(core->time->clock).microseconds / 1000000.0;
+        core->time->elapsed_time += core->time->delta_time;
+    } else if (core->menu->pause_on == TRUE && core->time->recreate_clock == FALSE) {
+        sfClock_destroy(core->time->clock);
+        core->time->recreate_clock = TRUE;
+    }
 }
--- Source/Game_core/wnd_open.c
+++ Source/Game_core/wnd_open.c
@@ -5,19 +5,20 @@
 ** window_open.c
 */
 
+#include <stdio.h>
 #include "../../include/defender.h"
 
 void wnd_open(core_t *core)
 {
+    static int y = 0;
     init(core);
     while (sfRenderWindow_isOpen(core->wnd->window)) {
+        y++;
         sfRenderWindow_display(core->wnd->window);
         if (core->menu->button->end_gm == TRUE &&
             core->menu->button->enable_click == FALSE)
             break;
-        if (core->menu->pause_on == FALSE) {
-            update_time(core->time);
-        }
+        update_time(core);
         poll_event(core->wnd, core->menu);
         print_scene(core->menu, core->wnd, core->game, core);
         switch_bg(core->menu, core->game->pause);
