34146709dd27cdc9f9c434f6b45efd680fa07db4
Tom <tom@localhost.localdomain>
2021-04-01 14:52:53 +0200

    ADD: function in my.h

--- include/my.h
+++ include/my.h
@@ -21,8 +21,11 @@ int my_strlen(char const *str);
 
 int read_map(duo_stp_t *map, char **av);
 int open_file(duo_stp_t *duo);
-int store_map(duo_stp_t *map);
-int which_file(duo_stp_t *duo);
 int cp_nbr(duo_stp_t *duo, int y);
+
+int find_arg(char **av);
+int store_nbrs(char **av, duo_stp_t *duo);
+void free_func(duo_stp_t *duo);
+int main(int ac, char **av);
 
 #endif /* !MY_H_ */
