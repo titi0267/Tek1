51d9271ebdd810d1d54b9d29bc57655909ae4fc4
Tom <tom@localhost.localdomain>
2021-04-01 16:08:03 +0200

    ADD: read

--- src/read.c
+++ src/read.c
@@ -7,7 +7,59 @@
 
 #include "../include/my.h"
 
-void print_digit(duo_stp_t *duo)
+void print_digit_five(duo_stp_t *duo)
+{
+    for (int i = 0; duo->store_buf[i] != NULL; i++) {
+        for (int d = 0; d != 5; d++) {
+            if (duo->store_buf[i][d] == '\n' && i != duo->len)
+                printf(" ");
+            else
+                printf("%c", duo->store_buf[i][d]);
+        }
+    }
+    printf("\n");
+}
+
+void print_digit_four(duo_stp_t *duo)
+{
+    for (int i = 0; duo->store_buf[i] != NULL; i++) {
+        for (int d = 0; d != 5; d++) {
+            if (duo->store_buf[i][d] == '\n' && i != duo->len)
+                printf(" ");
+            else
+                printf("%c", duo->store_buf[i][d]);
+        }
+    }
+    printf("\n");
+}
+
+void print_digit_three(duo_stp_t *duo)
+{
+    for (int i = 0; duo->store_buf[i] != NULL; i++) {
+        for (int d = 0; d != 5; d++) {
+            if (duo->store_buf[i][d] == '\n' && i != duo->len)
+                printf(" ");
+            else
+                printf("%c", duo->store_buf[i][d]);
+        }
+    }
+    printf("\n");
+}
+
+void print_digit_two(duo_stp_t *duo)
+{
+    for (int i = 0; duo->store_buf[i] != NULL; i++) {
+        for (int d = 0; d != 5; d++) {
+            if (duo->store_buf[i][d] == '\n' && i != duo->len)
+                print(" ");
+            else
+                printf("%c", duo->store_buf[i][d]);
+        }
+    }
+    printf("\n");
+}
+
+void print_digit_one(duo_stp_t *duo)
 {
     for (int i = 0; duo->store_buf[i] != NULL; i++) {
         for (int d = 0; d != 5; d++) {
