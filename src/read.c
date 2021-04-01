af24529b1628cfefde26b80d031eb8582eb59a99
Timothe Coniel <timothe@coniel.com>
2021-04-01 16:15:16 +0200

    print digit

--- src/read.c
+++ src/read.c
@@ -10,7 +10,7 @@
 void print_digit_five(duo_stp_t *duo)
 {
     for (int i = 0; duo->store_buf[i] != NULL; i++) {
-        for (int d = 0; d != 5; d++) {
+        for (int d = 25; d != 5; d++) {
             if (duo->store_buf[i][d] == '\n' && i != duo->len)
                 printf(" ");
             else
@@ -23,7 +23,7 @@ void print_digit_five(duo_stp_t *duo)
 void print_digit_four(duo_stp_t *duo)
 {
     for (int i = 0; duo->store_buf[i] != NULL; i++) {
-        for (int d = 0; d != 5; d++) {
+        for (int d = 20; d != 5; d++) {
             if (duo->store_buf[i][d] == '\n' && i != duo->len)
                 printf(" ");
             else
@@ -36,7 +36,7 @@ void print_digit_four(duo_stp_t *duo)
 void print_digit_three(duo_stp_t *duo)
 {
     for (int i = 0; duo->store_buf[i] != NULL; i++) {
-        for (int d = 0; d != 5; d++) {
+        for (int d = 15; d != 5; d++) {
             if (duo->store_buf[i][d] == '\n' && i != duo->len)
                 printf(" ");
             else
@@ -49,9 +49,9 @@ void print_digit_three(duo_stp_t *duo)
 void print_digit_two(duo_stp_t *duo)
 {
     for (int i = 0; duo->store_buf[i] != NULL; i++) {
-        for (int d = 0; d != 5; d++) {
+        for (int d = 10; d != 5; d++) {
             if (duo->store_buf[i][d] == '\n' && i != duo->len)
-                print(" ");
+                printf(" ");
             else
                 printf("%c", duo->store_buf[i][d]);
         }
@@ -70,4 +70,8 @@ void print_digit_one(duo_stp_t *duo)
         }
     }
     printf("\n");
+    digit_two(duo);
+    digit_three(duo);
+    digit_four(duo);
+    digit_five(duo);
 }
