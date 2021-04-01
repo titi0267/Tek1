39108a4b635935a63790d26aca1bb8798ef282c4
Tom <tom@localhost.localdomain>
2021-04-01 14:15:33 +0200

    CHANGE: my.h

--- include/my.h
+++ include/my.h
@@ -18,5 +18,9 @@ void my_putchar(char c);
 int my_putstr(char const *str);
 int my_getnbr(char *str);
 int my_strlen(char const *str);
+int read_map(duo_stp_t *map, char **av);
+int open_file(duo_stp_t *duo);
+int store_map(duo_stp_t *map);
+int which_file(duo_stp_t *duo);
 
 #endif /* !MY_H_ */
