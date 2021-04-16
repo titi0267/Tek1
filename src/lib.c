5f9689883695f58654c37162d35626ba239f96e8
Tom <tom@localhost.localdomain>
2021-04-16 15:19:51 +0200

    ADD: revstr and put in the my.h

--- src/lib.c
+++ src/lib.c
@@ -47,3 +47,19 @@ int my_strlen(char const *str)
     }
     return (i);
 }
+
+char my_revstr(char *str)
+{
+    int i = 0;
+    int len = my_strlen(str) -1;
+    char temp;
+
+    while (i <= len) {
+        temp = str[i];
+        str[i] = str[len];
+        str[len] = temp;
+        i++;
+        len--;
+    }
+    return (str);
+}
