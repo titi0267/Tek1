875fcb741720a09a3670503841436dca2001efe4
Tom <tom@localhost.localdomain>
2021-04-30 13:57:10 +0200

    ADD: error

--- src/error.c
+++ src/error.c
@@ -9,5 +9,20 @@
 
 int error_core(int ac, char **av, stumper_t *stp)
 {
+    error_argnbr(ac, av);
     return (0);
+}
+
+void error_argnbr(int ac, char **av)
+{
+    if (ac < 2) {
+        write(2, "missing arguemnts\n", 19);
+        exit(84);
+    }
+    if (ac == 2) {
+        if (ac[1][0] > 97 || ac[1][0] < 122) {
+            write(2, "must be a text\n", 16);
+            exit(84);
+        }
+    }
 }
