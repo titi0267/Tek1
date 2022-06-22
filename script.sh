02987c4b7d5fc69ce2c3a978d1aca4f6fbf1d51b
coniel <timothe@coniel.com>
2022-06-22 22:08:22 +0200

    Add: CPoolDays

--- script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-git checkout main
-git remote add Day08 git@github.com:EpitechIT2020/B-CPE-100-STG-1-1-cpoolday08-timothe.coniel.git
-git fetch Day08
-git checkout -b Day08 Day08/master
-git checkout CPoolDays
-git merge Day08 --allow-unrelated-histories
-mkdir Day08
